d108868af566d7e719752a155e044e6f5c45230a
Nudesuppe42 <jufo.theo@gmail.com>
2022-06-02 19:36:15 +0200

    Footer 2

---
+++ components/Page/Footer.tsx
@@ -0,0 +1,77 @@
+/* eslint-disable @next/next/no-img-element */
+
+import { Anchor, Container, Group, Menu, createStyles } from "@mantine/core";
+
+import React from "react";
+
+const useStyles = createStyles((theme) => ({
+  footer: {
+    marginTop: 120,
+    borderTop: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]
+    }`,
+  },
+
+  inner: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+
+    [theme.fn.smallerThan("xs")]: {
+      flexDirection: "column",
+    },
+  },
+
+  links: {
+    [theme.fn.smallerThan("xs")]: {
+      marginTop: theme.spacing.md,
+    },
+  },
+}));
+
+interface FooterSimpleProps {
+  links: { link: string; label: string }[];
+}
+
+export default function Footer({ links }: FooterSimpleProps) {
+  const { classes } = useStyles();
+  const items = links.map((link) => (
+    <Anchor<"a">
+      color="dimmed"
+      key={link.label}
+      href={link.link}
+      onClick={(event) => event.preventDefault()}
+      size="sm"
+    >
+      {link.label}
+    </Anchor>
+  ));
+
+  return (
+    <div className={classes.footer}>
+      <Container className={classes.inner} size={"xl"}>
+        <img src="/logo.gif" height="40" alt="Logo" />
+        <Group className={classes.links}>
+          {items}
+          <Menu
+          position="top"
+            control={
+              <Anchor<"a">
+                color="dimmed"
+                onClick={(event) => event.preventDefault()}
+                size="sm"
+              >
+                Language
+              </Anchor>
+            }
+          >
+              <Menu.Item icon={<img src="https://countryflagsapi.com/png/gb" height={"15"} />}>English</Menu.Item>
+              <Menu.Item icon={<img src="https://countryflagsapi.com/png/de" height={"15"} />}>German</Menu.Item>
+          </Menu>
+        </Group>
+      </Container>
+    </div>
+  );
+}
--- components/Page/index.tsx
+++ components/Page/index.tsx
@@ -1,3 +1,4 @@
+import Footer from "./Footer";
 import Header from "./Header";
 
 interface PageProps {
@@ -6,21 +7,33 @@ interface PageProps {
     name: string;
     avatar: string;
   };
+  disabled?: {
+    header?: boolean;
+    footer?: boolean;
+  }
 }
 
 const Page = (props: any) => {
   return (
     <div>
-      <Header
+      {!props.disabled?.header && <Header
         links={[
           { link: "faq", label: "FAQ" },
           { link: "map", label: "Map" },
           { link: "teams", label: "Build Teams" },
-          { link: "contact", label: "Contact"},
+          { link: "contact", label: "Contact" },
         ]}
         user={props.user}
-      />
+      />}
+      
       {props.children}
+      {!props.disabled?.footer &&<Footer
+        links={[
+          { link: "faq", label: "FAQ" },
+          { link: "contact", label: "Contact" },
+          { link: "about", label: "About us" },
+        ]}
+      />}
     </div>
   );
 };
