d3ce58d8913dc5fb67c3650e3b2e2ebc9442ef8e
Nudesuppe42 <jufo.theo@gmail.com>
2022-07-08 16:20:46 +0200

    feat(home): Initial Design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,95 @@
-import {Anchor} from '@mantine/core'
+import {ActionIcon, Box, Button, Center, Grid, Title, useMantineTheme} from '@mantine/core'
+
+import {ChevronDown} from 'tabler-icons-react'
 import {NextPage} from 'next'
 import Page from '../components/Page'
 import React from 'react'
+import {useRouter} from 'next/router'
 
 const Home: NextPage = () => {
+  const theme = useMantineTheme()
+  const router = useRouter()
   return (
     <Page
-      head={{
-        title: 'Example Head',
-        subtitle: 'Example Subtitle',
-        image: '/images/placeholder.png',
-        large: true
-      }}
       user={{
         name: 'Nudelsuppe_42_#3571',
         avatar: 'https://cdn.discordapp.com/avatars/635411595253776385/66a67aa69149c976f3b962d72ca17146.png'
       }}
+      fullWidth
     >
-      <h1>Example Heading 1</h1>
-      <h2>Example Heading 2</h2>
-      <h3>Example Heading 3</h3>
-      <h4>Example Heading 4</h4>
-      <h5>Example Heading 5</h5>
-      <h6>Example Heading 6</h6>
-      <p>Example Paragraph</p>
-      <Anchor>Example Anchor</Anchor>
+      <div
+        style={{
+          width: '100%',
+          height: 'calc(100vh - 60px)',
+          position: 'relative'
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+            background: `url("/images/placeholder.png")`,
+            filter: 'brightness(0.5)',
+            width: '100%',
+            height: '100%',
+            marginTop: 60
+          }}
+        ></div>
+        <Center
+          style={{
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            top: 60,
+            left: 0
+          }}
+        >
+          <Title style={{color: '#ffffff'}} align="center" order={1}>
+            Recreating The Earth In Minecraft
+            <br />
+            <Button
+              variant="outline"
+              size="xl"
+              style={{color: 'white', borderColor: 'white', borderWidth: 3, marginTop: theme.spacing.xl * 1.5}}
+              onClick={() => router.push('#start')}
+            >
+              Get Started
+            </Button>
+          </Title>
+        </Center>
+        <Center
+          style={{
+            width: '100%',
+            height: '0%',
+            position: 'absolute',
+            bottom: 0,
+            left: 0
+          }}
+        >
+          <ActionIcon<'a'>
+            styles={{root: {height: 64, width: 64}}}
+            radius="xs"
+            variant="transparent"
+            onClick={() => router.push('#1')}
+          >
+            <ChevronDown size={64} color="white" />
+          </ActionIcon>
+        </Center>
+      </div>
+      <Box style={{background: '#ffffff', marginTop: 60, overflow: 'hidden'}}>
+        <Grid>
+          <Grid.Col span={6}>
+            <h2>Our Grand Mission</h2>
+            <p>
+              Our mission is to fully recreate the entire Earth in Minecraft at a 1:1 scale. One block in Minecraft
+              equates to roughly one meter in the real world, meaning that this project will fully recreate the size of
+              our planet. Anyone is able to join us and contribute to the largest and most expansive build project to
+              ever have been attempted in Minecraft. Every language, nationality, and regional difference is accepted
+              and regarded as our greatest attribute as we continue our journey to unite all of Humanity&apos;s greatest
+              achievements into a single Minecraft world.
+            </p>
+          </Grid.Col>
+        </Grid>
+      </Box>
     </Page>
   )
 }
