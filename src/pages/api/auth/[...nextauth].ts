cb623caaa7764e4b330da79e4683071a26fd1f0d
Nudesuppe42 <jufo.theo@gmail.com>
2022-09-06 14:06:05 +0200

    chore(auth): Moved Keycloak options to .evn

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,9 +4,9 @@ import NextAuth from 'next-auth'
 export default NextAuth({
   providers: [
     KeycloakProvider({
-      clientId: 'dev',
-      clientSecret: 'atp3Xce20wI9un4ywmPV4NdUwv9dZJtV',
-      issuer: 'http://localhost:8080/realms/bte-dev'
+      clientId: process.env.KEYCLOAK_ID || '',
+      clientSecret: process.env.KEYCLOAK_SECRET || '',
+      issuer: process.env.KEYCLOAK_URL || ''
     })
   ],
   callbacks: {
