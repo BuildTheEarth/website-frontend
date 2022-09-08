d797adb88a8850668e66909b4b4b24bce040cb88
Nudesuppe42 <jufo.theo@gmail.com>
2022-09-08 13:44:42 +0200

    fix(footer): Reduced Footer height

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,57 +1,57 @@
-import {Anchor, Container, Group, Menu, createStyles} from '@mantine/core'
+import { Anchor, Container, Group, Menu, createStyles } from '@mantine/core';
 
-import React from 'react'
-import {LanguageSwitcher} from "./LanguageSwitcher";
+import { LanguageSwitcher } from './LanguageSwitcher';
+import React from 'react';
 
-const useStyles = createStyles(theme => ({
-  footer: {
-    borderTop: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]}`,
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : '#ffffff'
-  },
+const useStyles = createStyles((theme) => ({
+	footer: {
+		borderTop: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]}`,
+		backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : '#ffffff',
+	},
 
-  inner: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingTop: theme.spacing.xl,
-    paddingBottom: theme.spacing.xl,
+	inner: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		paddingTop: theme.spacing.sm,
+		paddingBottom: theme.spacing.sm,
 
-    [theme.fn.smallerThan('xs')]: {
-      flexDirection: 'column'
-    }
-  },
+		[theme.fn.smallerThan('xs')]: {
+			flexDirection: 'column',
+		},
+	},
 
-  links: {
-    [theme.fn.smallerThan('xs')]: {
-      marginTop: theme.spacing.md
-    }
-  }
-}))
+	links: {
+		[theme.fn.smallerThan('xs')]: {
+			marginTop: theme.spacing.md,
+		},
+	},
+}));
 
 interface FooterSimpleProps {
-  links: {link: string; label: string}[]
-  style?: React.CSSProperties
+	links: { link: string; label: string }[];
+	style?: React.CSSProperties;
 }
 
-export default function Footer({links, style}: FooterSimpleProps) {
-  const {classes} = useStyles()
-  const items = links.map(link => (
-    <Anchor<'a'> color="dimmed" key={link.label} href={link.link} size="sm">
-      {link.label}
-    </Anchor>
-  ))
+export default function Footer({ links, style }: FooterSimpleProps) {
+	const { classes } = useStyles();
+	const items = links.map((link) => (
+		<Anchor<'a'> color="dimmed" key={link.label} href={link.link} size="sm">
+			{link.label}
+		</Anchor>
+	));
 
-  return (
-    <div className={classes.footer} style={style}>
-      <Container className={classes.inner} size={'xl'}>
-        <Anchor<'a'> color="dimmed" variant="text">
-          &copy; {new Date().getFullYear()} BuildTheEarth
-        </Anchor>
-        <Group className={classes.links}>
-          {items}
-          <LanguageSwitcher />
-        </Group>
-      </Container>
-    </div>
-  )
+	return (
+		<div className={classes.footer} style={style}>
+			<Container className={classes.inner} size={'xl'}>
+				<Anchor<'a'> color="dimmed" variant="text">
+					&copy; {new Date().getFullYear()} BuildTheEarth
+				</Anchor>
+				<Group className={classes.links}>
+					{items}
+					<LanguageSwitcher />
+				</Group>
+			</Container>
+		</div>
+	);
 }
