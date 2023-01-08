4d62edd24b5c2d30e18a3779d3ca315393596f37
Nudesuppe42 <jufo.theo@gmail.com>
2023-01-08 21:45:29 +0100

    fix(locale):

--- ni18n.config.ts
+++ ni18n.config.ts
@@ -4,7 +4,7 @@ import type { Ni18nOptions } from 'ni18n';
 import { languages } from './src/components/LanguageSwitcher';
 
 export const ni18nConfig: Ni18nOptions = {
-	fallbackLng: 'gb',
-	supportedLngs: ['gb', 'de'],
+	fallbackLng: 'en',
+	supportedLngs: ['en', 'de'],
 	ns: ['common', 'errors', 'home'],
 };
