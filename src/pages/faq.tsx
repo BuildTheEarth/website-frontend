f47bd2d2ab448817b66596fca6662c107b5679e1
Robin Ferch <robin@ferch-mail.de>
2023-03-20 20:22:01 +0100

    :sparkles: feat(faq): Add faq frontend

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,53 +1,13 @@
-import { Accordion, createStyles } from '@mantine/core';
+import {Accordion, createStyles, Skeleton} from '@mantine/core';
 
 import { NextPage } from 'next';
 import Page from '../components/Page';
 import React from 'react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
+import useSWR from "swr";
+import sanitizeHtml from 'sanitize-html';
 
-const elements = [
-	{
-		title: 'How can I start building, what version and settings do I need?',
-		content:
-			'To build on this project, you need to have a legitimate copy of Minecraft: Java Edition installed. Next, download and run the appropriate installer for your OS. Windows Installer (ZIP) macOS Installer (DMG) Linux Executable (TAR) Universal Installer (JAR) If you encounter any issues, hop onto the #support channel on our Discord!',
-	},
-	{
-		title: 'How can we support the project financially?',
-		content:
-			'To build on this project, you need to have a legitimate copy of Minecraft: Java Edition installed. Next, download and run the appropriate installer for your OS. Windows Installer (ZIP) macOS Installer (DMG) Linux Executable (TAR) Universal Installer (JAR) If you encounter any issues, hop onto the #support channel on our Discord!',
-	},
-	{
-		title: 'I am having crashes on startup, run out of RAM?',
-		content:
-			'To build on this project, you need to have a legitimate copy of Minecraft: Java Edition installed. Next, download and run the appropriate installer for your OS. Windows Installer (ZIP) macOS Installer (DMG) Linux Executable (TAR) Universal Installer (JAR) If you encounter any issues, hop onto the #support channel on our Discord!',
-	},
-	{
-		title: 'Is there a Multiplayer server for this project?',
-		content:
-			'To build on this project, you need to have a legitimate copy of Minecraft: Java Edition installed. Next, download and run the appropriate installer for your OS. Windows Installer (ZIP) macOS Installer (DMG) Linux Executable (TAR) Universal Installer (JAR) If you encounter any issues, hop onto the #support channel on our Discord!',
-	},
-	{
-		title: 'Roads don`t show up',
-		content:
-			'To build on this project, you need to have a legitimate copy of Minecraft: Java Edition installed. Next, download and run the appropriate installer for your OS. Windows Installer (ZIP) macOS Installer (DMG) Linux Executable (TAR) Universal Installer (JAR) If you encounter any issues, hop onto the #support channel on our Discord!',
-	},
-	{
-		title: 'How do I teleport to a specific place?',
-		content:
-			'To build on this project, you need to have a legitimate copy of Minecraft: Java Edition installed. Next, download and run the appropriate installer for your OS. Windows Installer (ZIP) macOS Installer (DMG) Linux Executable (TAR) Universal Installer (JAR) If you encounter any issues, hop onto the #support channel on our Discord!',
-	},
-	{
-		title: 'Install Java for mismatching architectures',
-		content:
-			'To build on this project, you need to have a legitimate copy of Minecraft: Java Edition installed. Next, download and run the appropriate installer for your OS. Windows Installer (ZIP) macOS Installer (DMG) Linux Executable (TAR) Universal Installer (JAR) If you encounter any issues, hop onto the #support channel on our Discord!',
-	},
-	{
-		title: 'Why am I spawning on a mushroom island?',
-		content:
-			'To build on this project, you need to have a legitimate copy of Minecraft: Java Edition installed. Next, download and run the appropriate installer for your OS. Windows Installer (ZIP) macOS Installer (DMG) Linux Executable (TAR) Universal Installer (JAR) If you encounter any issues, hop onto the #support channel on our Discord!',
-	},
-];
 const useStyles = createStyles((theme, _params, getRef) => {
 	const control = getRef('control');
 
@@ -69,6 +29,7 @@ const useStyles = createStyles((theme, _params, getRef) => {
 const Faq: NextPage = () => {
 	const { classes } = useStyles();
 	const { t } = useTranslation('faq');
+	const { data } = useSWR(`/faq`);
 	return (
 		<Page
 			head={{
@@ -77,11 +38,21 @@ const Faq: NextPage = () => {
 				large: true,
 			}}
 		>
+			{
+				!data &&
+				new Array(5).fill(0).map(() => {
+					return (
+						<Skeleton height={50} my={"md"}/>
+					)
+				})
+			}
 			<Accordion variant="separated">
-				{elements.map((element, idx) => (
-					<Accordion.Item value={element.title} key={idx}>
-						<Accordion.Control>{element.title}</Accordion.Control>
-						<Accordion.Panel>{element.content}</Accordion.Panel>
+				{data?.map((element: any) => (
+					<Accordion.Item value={element.id} key={element.id}>
+						<Accordion.Control>{element.question}</Accordion.Control>
+						<Accordion.Panel>
+							<div dangerouslySetInnerHTML={{__html: sanitizeHtml(element.answer)}} />
+						</Accordion.Panel>
 					</Accordion.Item>
 				))}
 			</Accordion>
