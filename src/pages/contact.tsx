0279d606e7cb98d3a9358d31bdf67a82167f3950
Nudesuppe42 <jufo.theo@gmail.com>
2023-04-02 14:24:45 +0200

    feat(contact): ✨ Integrate Backend

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -18,46 +18,14 @@ import Page from '../components/Page';
 import React from 'react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
+import useSWR from 'swr';
 import { useTranslation } from 'next-i18next';
-
-const contacts = [
-	{
-		name: 'Suchet M.',
-		avatar: 'https://cdn.discordapp.com/avatars/172308595046744064/5c22f8f891158e3221a1488b47f9e817.png',
-		position: 'CMO / Business Inq. Intl. / Media',
-		mail: 'suchet@buildtheearth.net',
-		discord: 'Suchet#7082',
-		discordId: '172308595046744064',
-	},
-	{
-		name: 'Bernard M.',
-		avatar: 'https://cdn.discordapp.com/avatars/310185776748953600/46d18c50e2ff900750749ff18005588f.png',
-		position: 'Chief Revenue Officer / Business Inquiries',
-		mail: 'boaz@buildtheearth.net',
-		discord: 'Knish#8000',
-		discordId: '310185776748953600',
-	},
-	{
-		name: 'Chang Y.',
-		avatar: 'https://cdn.discordapp.com/avatars/223918685750951939/2adfcf049cd63f27a950138fed2decf6.png',
-		position: 'Claims',
-		mail: 'c.yu@buildtheearth.net',
-		discord: 'saltypotato#3476',
-		discordId: '223918685750951939',
-	},
-	{
-		name: 'MrSmarty',
-		avatar: 'https://cdn.discordapp.com/avatars/671594544089006091/90cb31c7b674f56d1ec8e411ea9dd645.png',
-		position: 'Community',
-		mail: 'mrsmarty@buildtheearth.net',
-		discord: 'MrSmarty#1732',
-		discordId: '671594544089006091',
-	},
-];
 
 const Contact: NextPage = () => {
 	const router = useRouter();
+	const { data } = useSWR('/contacts');
 	const theme = useMantineTheme();
+
 	return (
 		<Page
 			head={{
@@ -68,13 +36,13 @@ const Contact: NextPage = () => {
 		>
 			<h3 style={{ textAlign: 'center', fontSize: '30px', lineHeight: '0' }}>Individual People</h3>
 			<Grid mt="md">
-				{contacts.map((contact) => (
-					<Grid.Col key={contact.name} sm={6}>
+				{data?.map((contact: any) => (
+					<Grid.Col key={contact.id} sm={6}>
 						<Group noWrap>
-							<Avatar src={contact.avatar} size={94} />
+							<Avatar src={contact.avatar} size={94} style={{ filter: 'invert(0)' }} />
 							<div>
 								<Text size="xs" sx={{ textTransform: 'uppercase' }} weight={700} color="dimmed">
-									{contact.position}
+									{contact.role}
 								</Text>
 								<Text size="lg" weight={500}>
 									{contact.name}
@@ -89,8 +57,13 @@ const Contact: NextPage = () => {
 
 								<Group noWrap spacing={10} mt={5}>
 									<At size={16} />
-									<Text size="xs" color="dimmed" onClick={() => window.open(`mailto:${contact.mail}`, '_blank')}>
-										{contact.mail}
+									<Text
+										size="xs"
+										color="dimmed"
+										style={{ cursor: 'pointer' }}
+										onClick={() => window.open(`mailto:${contact.mail}`, '_blank')}
+									>
+										{contact.email}
 									</Text>
 								</Group>
 							</div>
