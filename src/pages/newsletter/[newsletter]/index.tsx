7e3e8199bdfc3e0696b414d86333ff22d3d5d6ce
Nudesuppe42 <jufo.theo@gmail.com>
2023-06-16 14:40:13 +0200

    feat(newsletter): ✨ Thumpnail on List Page, removed back button

--- src/pages/newsletter/[newsletter]/index.tsx
+++ src/pages/newsletter/[newsletter]/index.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-undef */
 
-import { ArrowNarrowLeft } from 'tabler-icons-react';
-import { Group, useMantineTheme, Image, ActionIcon } from '@mantine/core';
+import { ActionIcon, Group, Image, useMantineTheme } from '@mantine/core';
+import React, { useState } from 'react';
 
+import { ArrowNarrowLeft } from 'tabler-icons-react';
 import { NextPage } from 'next';
 import Page from '../../../components/Page';
-import React, { useState } from 'react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
@@ -14,8 +14,8 @@ const Newsletter: NextPage = () => {
 	const router = useRouter();
 	const theme = useMantineTheme();
 	const [activePage, setPage] = useState(1);
-    const newsletter = router.query.newsletter;
-    const { data } = useSWR(`/newsletter/${newsletter}?isIssue=true`);
+	const newsletter = router.query.newsletter;
+	const { data } = useSWR(`/newsletter/${newsletter}?isIssue=true`);
 	console.log(data);
 
 	return (
@@ -24,23 +24,15 @@ const Newsletter: NextPage = () => {
 				title: data?.title,
 				image: data?.links[0] || '/images/placeholder.webp',
 				large: true,
+				subtitle: `Issue ${data?.issue}, ${new Date(data?.published_date).toLocaleDateString()}`,
 			}}
 		>
-			<Group position="center" pt="md" style={{padding: "1rem"}}>
-			<ActionIcon color="blue" size="xl" variant="outline" onClick={() => router.push(`/newsletter`)}>
-					<ArrowNarrowLeft size={18} />
-			</ActionIcon>
-			</Group>
-			<Group style={{width: "100%"}}>
-				{
-					data?.links.map((link: any) => (
-						<Group key={link.id} p="1rem" style={{width: "100%"}}>
-							<Image src={link} alt={data.title} />
-						</Group>
-					))
-					
-				}
-
+			<Group style={{ width: '100%' }}>
+				{data?.links.slice(1).map((link: any) => (
+					<Group key={link.id} py="1rem" style={{ width: '100%' }}>
+						<Image src={link} alt={data.title} />
+					</Group>
+				))}
 			</Group>
 		</Page>
 	);
--- src/pages/newsletter/index.tsx
+++ src/pages/newsletter/index.tsx
@@ -1,19 +1,41 @@
 /* eslint-disable no-undef */
 
-import { At, ExternalLink } from 'tabler-icons-react';
-import { Avatar, Box, Button, Center, Grid, Group, Pagination, Text, ThemeIcon, useMantineTheme } from '@mantine/core';
+import { At, Calendar, ExternalLink, Number } from 'tabler-icons-react';
+import {
+	Avatar,
+	Box,
+	Button,
+	Center,
+	Grid,
+	Group,
+	Pagination,
+	Text,
+	ThemeIcon,
+	createStyles,
+	useMantineTheme,
+} from '@mantine/core';
+import React, { useState } from 'react';
 
 import { NextPage } from 'next';
 import Page from '../../components/Page';
-import React, { useState } from 'react';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import { useTranslation } from 'next-i18next';
 
+const useStyles = createStyles((theme) => ({
+	icon: {
+		color: theme.colorScheme === 'dark' ? theme.colors.dark[3] : theme.colors.gray[5],
+	},
+
+	name: {
+		fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+	},
+}));
 const NewsletterList: NextPage = () => {
 	const router = useRouter();
 	const theme = useMantineTheme();
+	const { classes } = useStyles();
 	const { t } = useTranslation('newsletter');
 	const [activePage, setPage] = useState(1);
 	const { data } = useSWR(`/newsletter?page=${activePage - 1}`);
@@ -26,30 +48,47 @@ const NewsletterList: NextPage = () => {
 				large: true,
 			}}
 		>
-			<Group px="auto"  >
-				{
-					data?.data.map((newsletter: any) => (
-						<Box key={newsletter.id} p="1rem" px="auto" style={{width: "100%"}}>
-							<Grid>
-									<Grid.Col span={10}>
-										<Text size="xl">{new Date(newsletter?.published_date).toLocaleString("default", {month: "long", year: "numeric"})}</Text>
-										<Text size="sm">{newsletter?.title}</Text>
-									</Grid.Col>
-									<Grid.Col span={2}>
-										<Button
-											variant="outline"
-											color="blue"
-											size="sm"
-											onClick={() => router.push(`/newsletter/${newsletter?.issue}`)}
-											style={{top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}
-										>
-											<ExternalLink size={18} />
-										</Button>
-									</Grid.Col>
-							</Grid>
-						</Box>
-					))
-				}
+			<p>for here</p>
+			<Group px="auto" mt="xl">
+				{data?.data.map((newsletter: any, i: number) => (
+					<Group
+						noWrap
+						sx={{
+							backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[1],
+							borderRadius: theme.radius.xs,
+							'&:hover': {
+								backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[4],
+							},
+							width: '100%',
+						}}
+						key={`i-${i}`}
+						p="md"
+						onClick={() => router.push(`/newsletter/${newsletter.issue}`)}
+					>
+						<Avatar src={newsletter.links[0]} size={94} radius="md" />
+						<div>
+							<Group position="apart">
+								<Text size="lg" weight={500} className={classes.name}>
+									{newsletter.title}
+								</Text>
+							</Group>
+
+							<Group noWrap spacing={10} mt={3}>
+								<Number size={16} className={classes.icon} />
+								<Text size="xs" color="dimmed">
+									Issue {newsletter.issue}
+								</Text>
+							</Group>
+
+							<Group noWrap spacing={10} mt={5}>
+								<Calendar size={16} className={classes.icon} />
+								<Text size="xs" color="dimmed">
+									{new Date(newsletter.published_date).toLocaleDateString()}
+								</Text>
+							</Group>
+						</div>
+					</Group>
+				))}
 			</Group>
 			<Group position="center" pt="md">
 				<Pagination total={data?.pages || 1} radius="xs" page={activePage} onChange={setPage} siblings={1} />
