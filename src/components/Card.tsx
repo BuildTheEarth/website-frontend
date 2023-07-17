9924addc1f3d1692288c134ebe57d1b52eb22134
Nudesuppe42 <jufo.theo@gmail.com>
2023-07-17 21:10:56 +0200

    feat(getstarted): :bulb: Page Ideas

---
+++ src/components/Card.tsx
@@ -0,0 +1,83 @@
+import { Button, Paper, Text, Title, createStyles } from '@mantine/core';
+
+import { R } from '@icons-pack/react-simple-icons';
+
+const useStyles = createStyles((theme) => ({
+	card: {
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+		alignItems: 'flex-start',
+		backgroundSize: 'cover',
+		backgroundPosition: 'center',
+	},
+
+	title: {
+		fontWeight: 900,
+		color: theme.white,
+		lineHeight: 1.2,
+		marginTop: theme.spacing.xs,
+	},
+
+	category: {
+		color: theme.white,
+		opacity: 0.7,
+		fontWeight: 700,
+		textTransform: 'uppercase',
+	},
+}));
+
+interface ArticleCardImageProps {
+	image: string;
+	title: string;
+	category: string;
+	style?: any;
+	showButton?: boolean;
+	buttonText?: string;
+	onClick?: () => void;
+}
+
+export function BackgroundCard({
+	image,
+	title,
+	category,
+	style,
+	showButton,
+	buttonText,
+	onClick,
+}: ArticleCardImageProps) {
+	const { classes } = useStyles();
+
+	return (
+		<Paper
+			shadow="md"
+			p="xl"
+			radius="md"
+			sx={{ backgroundImage: `url(${image})`, position: 'relative', ...style }}
+			className={classes.card}
+		>
+			<div style={{ zIndex: 8 }}>
+				<Text className={classes.category} size="xs">
+					{category}
+				</Text>
+				<Title order={3} className={classes.title}>
+					{title}
+				</Title>
+			</div>
+			{showButton && (
+				<Button variant="white" color="dark" size="md" onClick={onClick}>
+					{buttonText}
+				</Button>
+			)}
+			<div
+				style={{
+					position: 'absolute',
+					top: -1,
+					left: 0,
+				}}
+			>
+				<img src="/galleryoverlay.svg" alt="" width={'100%'} style={{ rotate: '180deg', borderRadius: '8px' }} />
+			</div>
+		</Paper>
+	);
+}
--- src/pages/getstarted/index.tsx
+++ src/pages/getstarted/index.tsx
@@ -2,6 +2,7 @@ import { Alert, Button, Center, Grid, Text, useMantineTheme } from '@mantine/cor
 import React, { useState } from 'react';
 
 import { AlertCircle } from 'tabler-icons-react';
+import { BackgroundCard } from '../../components/Card';
 import { NextPage } from 'next';
 import Page from '../../components/Page';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
@@ -14,7 +15,7 @@ const GetStarted: NextPage = () => {
 	const { t } = useTranslation('getstarted');
 	const router = useRouter();
 	const largeScreen = useMediaQuery('(min-width: 900px)');
-	const [focused, setFocused] = useState<number | null>(null);
+	const [focused, setFocused] = useState<number>(1);
 	return (
 		<Page
 			head={{
@@ -23,94 +24,32 @@ const GetStarted: NextPage = () => {
 				large: true,
 			}}
 		>
-			{' '}
-			{/* TODO: yea, i think you know \/ */}
-			<Alert icon={<AlertCircle size={16} />} title="Warning" color="red" variant="filled">
-				This content is out of date. We are aware of this and will update it soon. Please dont add issues about this.
-			</Alert>
 			<p>{t('description')}</p>
-			<Grid mt="lg" style={{ minHeight: '60vh' }}>
-				<Grid.Col
-					sm={focused ? (focused === 1 ? 8 : 4) : 6}
-					onMouseEnter={() => setFocused(1)}
-					style={{
-						backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
-						background: `url("/images/getstarted/network.webp") center center / cover`,
-						transition: 'all 0.2s ease-out',
-						padding: 0,
-					}}
-				>
-					<Center style={{ width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-						<div style={{ color: '#ffffff', textAlign: 'center' }}>
-							<h1>{t('network.title')}</h1>
-							<p>{t('network.description')}</p>
-							<div style={{ opacity: largeScreen && focused !== 1 ? 0 : 1, transition: 'all 0.2s ease-out' }}>
-								<h4>{t('compability')}</h4>
-								<p>
-									{t('network.compability.java')}
-									<br />
-									{t('network.compability.bedrock')}
-								</p>
-								<Button
-									variant="outline"
-									size="lg"
-									style={{
-										color: 'white',
-										borderColor: 'white',
-										borderWidth: 3,
-										marginTop: theme.spacing.xl * 1.5,
-										marginBottom: theme.spacing.md,
-									}}
-									onClick={() => router.push('/getstarted/network')}
-								>
-									{t('network.getStarted')}
-								</Button>
-							</div>
-						</div>
-					</Center>
+			<Grid style={{ height: '50vh' }}>
+				<Grid.Col span={6} onMouseOver={() => setFocused(1)}>
+					<BackgroundCard
+						image="/images/getstarted/buildteams.webp"
+						title="Explore BuildTheEarth"
+						category="visiting"
+						style={{ height: '100%' }}
+						showButton={focused == 1}
+						buttonText="Select"
+					/>
 				</Grid.Col>
-				<Grid.Col
-					sm={focused ? (focused === 2 ? 8 : 4) : 6}
-					onMouseEnter={() => setFocused(2)}
-					style={{
-						backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
-						background: `url("/images/getstarted/buildteams.webp") center center / cover`,
-						transition: 'all 0.2s ease-out',
-						padding: 0,
-					}}
-				>
-					<Center style={{ width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
-						<div style={{ color: '#ffffff', textAlign: 'center' }}>
-							<h1>{t('teams.title')}</h1>
-							<p>{t('teams.description')}</p>
-							<div style={{ opacity: largeScreen && focused !== 2 ? 0 : 1, transition: 'all 0.2s ease-out' }}>
-								<h4>{t('compability')}</h4>
-								<p>
-									{t('teams.compability.java')}
-									<br />
-									{t('teams.compability.bedrock')}
-								</p>
-								<Button
-									variant="outline"
-									size="lg"
-									style={{
-										color: 'white',
-										borderColor: 'white',
-										borderWidth: 3,
-										marginTop: theme.spacing.xl * 1.5,
-										marginBottom: theme.spacing.md,
-									}}
-									onClick={() => router.push('/teams')}
-								>
-									{t('teams.getStarted')}
-								</Button>
-							</div>
-						</div>
-					</Center>
+				<Grid.Col span={6} onMouseOver={() => setFocused(2)}>
+					<BackgroundCard
+						image="/images/getstarted/network.webp"
+						title="Start Building"
+						category="building"
+						style={{ height: '100%' }}
+						showButton={focused == 2}
+						buttonText="Select"
+					/>
 				</Grid.Col>
 			</Grid>
+
 			<Text mt="lg">
-				<Text color="red" span inherit>
+				<Text color="red" span inherit pr="sm">
 					*
 				</Text>
 				No cost, no subscription, you can leave/switch at any point
