0560b30875377ddf07445f1f0b08ec309c45fe8f
Nudesuppe42 <jufo.theo@gmail.com>
2023-08-14 16:37:45 +0200

    style(src): :art: Prettier and Lint

--- README.md
+++ README.md
@@ -13,7 +13,6 @@ _The Website for the Build The Earth Project._
 </div>
 <!-- markdownlint-restore -->
 
-
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- next.config.js
+++ next.config.js
@@ -2,13 +2,13 @@
 const nextConfig = {
 	reactStrictMode: true,
 	images: {
-		domains: ['cdn.discordapp.com', "i.imgur.com"],
+		domains: ['cdn.discordapp.com', 'i.imgur.com'],
 	},
 	i18n: {
 		defaultLocale: 'en',
-		locales: ['en', 'de', 'es', 'fr', 'ru', 'nl']
+		locales: ['en', 'de', 'es', 'fr', 'ru', 'nl'],
 	},
 	output: 'standalone',
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
