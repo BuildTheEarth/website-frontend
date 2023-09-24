4046a0cbfea65e9ec6826b9dcc490c915ee66202
Nudesuppe42 <jufo.theo@gmail.com>
2023-09-24 16:44:56 +0200

    feat(home): :art: Index page rework v1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,11 @@ import {
 	useMantineColorScheme,
 	useMantineTheme,
 } from '@mantine/core';
+import { ChevronDown, ChevronRight } from 'tabler-icons-react';
 import { motion, useScroll, useTransform } from 'framer-motion';
 
-import { ChevronDown } from 'tabler-icons-react';
 import Gallery from '../components/Gallery';
+import { IconArrowRight } from '@tabler/icons';
 import Image from 'next/image';
 import Link from 'next/link';
 import { NextPage } from 'next';
@@ -32,8 +33,9 @@ const Home: NextPage = () => {
 	const { t } = useTranslation('home');
 	const router = useRouter();
 	const headBgPosY = useTransform(scrollYProgress, (latest) => `${latest * 5 + 50}%`);
-	const missionY = useTransform(scrollYProgress, [0, 0.8], ['45%', '0%']);
-	const galleryY = useTransform(scrollYProgress, [0.4, 0.8], ['45%', '0%']);
+	const missionY = useTransform(scrollYProgress, [0, 1], ['45%', '0%']);
+	const galleryY = useTransform(scrollYProgress, [0, 1], ['45%', '0%']);
+	const getstartedY = useTransform(scrollYProgress, [0, 1], ['45%', '0%']);
 	return (
 		<Page fullWidth title="Home">
 			<motion.div
@@ -60,12 +62,14 @@ const Home: NextPage = () => {
 						<Button
 							component={Link}
 							size="xl"
-							radius="xl"
-							variant="gradient"
+							variant="outline"
+							color="gray"
 							mt="md"
 							style={{
 								width: 'fit-content',
 								alignSelf: 'center',
+								textShadow: '0px 0px 28px #000',
+								borderWidth: '2px',
 							}}
 							href="/getstarted"
 						>
@@ -116,7 +120,13 @@ const Home: NextPage = () => {
 									}}
 								/>
 								<p>{t('mission.content')}</p>
-								<Button px={'calc(var(--mantine-spacing-xl)*2)'} component={Link} href="/about" mt="md">
+								<Button
+									px={'var(--mantine-spacing-xl)'}
+									component={Link}
+									href="/about"
+									mt="md"
+									rightSection={<ChevronRight />}
+								>
 									{t('mission.action')}
 								</Button>
 							</div>
@@ -188,134 +198,25 @@ const Home: NextPage = () => {
 							/>
 						</motion.div>
 					</Grid.Col>
-					<Grid.Col span={{ lg: 5 }} style={{ marginTop: 150, marginBottom: 150, minHeight: '30vh' }}>
-						<Center style={{ width: '100%', height: '100%' }}>
-							<div style={{ padding: '0px 10%' }}>
-								<h1 id="more" style={{ scrollMarginTop: 70 }}>
-									{t('mission.title')}
-								</h1>
-								<div
-									style={{
-										background: `linear-gradient(90deg, rgba(${
-											scheme.colorScheme === 'dark' ? '193, 194, 197' : '0, 0, 0'
-										},1) 20%, rgba(0,0,0,0) 20%)`,
-										height: 2,
-									}}
-								/>
-								<p>{t('mission.content')}</p>
-								<Button px={'calc(var(--mantine-spacing-xl)*2)'} component={Link} href="/about" mt="md">
-									{t('mission.action')}
-								</Button>
-							</div>
-						</Center>
-					</Grid.Col>
-					<Grid.Col
-						span={{ lg: 7 }}
-						style={{ marginTop: 150, marginBottom: 150, minHeight: '30vh', position: 'relative' }}
-					>
+					<Grid.Col span={{ lg: 6 }} style={{ minHeight: '30vh', position: 'relative', marginTop: 150 }}>
 						<motion.div
 							style={{
 								backgroundColor: scheme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
-								background: `url("https://cdn.buildtheearth.net/static/home/mission.webp") center center / cover`,
-								y: missionY,
-								marginLeft: '10%',
-								width: '60%',
-								height: '100%',
+								background: `url("https://cdn.buildtheearth.net/static/home/getstarted.webp") center center / cover`,
+								y: getstartedY,
+								marginLeft: '25%',
+								width: '65%',
+								height: '75%',
 								boxShadow: '10px 10px 0px 4px rgba(0,0,0,0.45)',
 							}}
 						/>
 					</Grid.Col>
-				</Grid>
-			</Box>
-			{/* <Box
-				style={{
-					background: scheme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
-					overflow: 'hidden',
-				}}
-			>
-				<Grid>
-					<Grid.Col span={{ lg: 5 }} style={{ marginTop: 50, marginBottom: 50, minHeight: '30vh' }}>
+					<Grid.Col span={{ lg: 5 }} style={{ marginTop: 150, marginBottom: 50, minHeight: '30vh' }}>
 						<Center style={{ width: '100%', height: '100%' }}>
 							<div style={{ padding: '0px 10%' }}>
 								<h1 id="more" style={{ scrollMarginTop: 70 }}>
-									{t('mission.title')}
+									{t('join.title')}
 								</h1>
-								<div
-									style={{
-										background: `linear-gradient(90deg, rgba(${
-											scheme.colorScheme === 'dark' ? '193, 194, 197' : '0, 0, 0'
-										},1) 20%, rgba(0,0,0,0) 20%)`,
-										height: 2,
-									}}
-								/>
-								<p>{t('mission.content')}</p>
-								<Button px={'calc(var(--mantine-spacing-xl)*2)'} component={Link} href="/about" mt="md">
-									{t('mission.action')}
-								</Button>
-							</div>
-						</Center>
-					</Grid.Col>
-					{/* TODO 
-					{/* <MediaQuery smallerThan="lg" styles={{ display: 'none' }}> 
-					<Grid.Col span={{ lg: 7 }} style={{ padding: 0 }}>
-						<BackgroundImage
-							src="https://cdn.buildtheearth.net/static/home/mission.webp"
-							style={{ width: '100%', height: '100%' }}
-						/>
-					</Grid.Col>
-					{/* </MediaQuery> 
-
-					<Grid.Col span={{ sm: 12 }} style={{ padding: 0, margin: 0 }}>
-						<Gallery
-							style={{ height: '70vh' }}
-							images={[
-								{
-									builder: 'Criffane 14#7255',
-									location: 'Krasnodar Park',
-									src: 'https://cdn.discordapp.com/attachments/811604280293064714/846615357585752084/image0.jpg',
-									country: 'ru',
-								},
-								{
-									builder: 'BTE Germany',
-									location: 'Schloss Drachenburg',
-									src: 'https://cdn.discordapp.com/attachments/692849007038562434/964097226341244988/4final2k_1.png',
-									country: 'de',
-								},
-								{
-									builder: 'mcnoided#4059',
-									location: 'Viaduc de Millau',
-									src: 'https://cdn.discordapp.com/attachments/811604280293064714/824403619587031070/Capture2.png',
-									country: 'fr',
-								},
-								{
-									builder: 'De leted#2098',
-									location: 'Wisconsin State Capitol',
-									src: 'https://cdn.discordapp.com/attachments/811604280293064714/992899056361799680/Capitol5.png',
-									country: 'us',
-								},
-								{
-									builder: 'BTE NYC',
-									location: 'Tribeca Seafront, NYC',
-									src: 'https://cdn.discordapp.com/attachments/714797791913705472/975523840652378162/seafront_2.png',
-									country: 'us',
-								},
-							]}
-						/>
-					</Grid.Col>
-					{/* TODO 
-					{/* <MediaQuery smallerThan="lg" styles={{ display: 'none' }}> 
-					<Grid.Col span={{ lg: 7 }} style={{ padding: 0 }}>
-						<BackgroundImage
-							src="https://cdn.buildtheearth.net/static/home/getstarted.webp"
-							style={{ width: '100%', height: '100%' }}
-						/>
-					</Grid.Col>
-					{/* </MediaQuery> 
-
-					<Grid.Col span={{ lg: 5 }} style={{ marginTop: 50, marginBottom: 50, minHeight: '30vh' }}>
-						<Center style={{ width: '100%', height: '100%' }}>
-							<div style={{ padding: '0px 10%' }}>
-								<h1>{t('join.title')}</h1>
 								<div
 									style={{
 										background: `linear-gradient(90deg, rgba(${
@@ -325,14 +226,20 @@ const Home: NextPage = () => {
 									}}
 								/>
 								<p>{t('join.content')}</p>
-								<Button px={'calc(var(--mantine-spacing-xl)*2)'} component={Link} href="/getstarted" mt="md">
+								<Button
+									px={'var(--mantine-spacing-xl)'}
+									component={Link}
+									href="/getstarted"
+									mt="md"
+									rightSection={<ChevronRight />}
+								>
 									{t('join.action')}
 								</Button>
 							</div>
 						</Center>
 					</Grid.Col>
 				</Grid>
-			</Box> */}
+			</Box>
 		</Page>
 	);
 };
