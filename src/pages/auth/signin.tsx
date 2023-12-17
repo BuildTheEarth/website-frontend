2491bcf7aa2d29da729184067235b9ff8d25a895
Nudesuppe42 <jufo.theo@gmail.com>
2023-12-17 11:40:55 +0100

    fix(auth): :bug: Use translations on login page

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,7 +1,8 @@
-import { Button, useMantineTheme } from '@mantine/core';
+import { Button, Code, Text, useMantineTheme } from '@mantine/core';
 
 import { NextPage } from 'next';
 import { signIn } from 'next-auth/react';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useRouter } from 'next/router';
 import Page from '../../components/Page';
 
@@ -21,17 +22,30 @@ const SignIn: NextPage = () => {
 		>
 			<h2>Sign in to BuildTheEarth</h2>
 			<p>
-				On your My BuildTheEarth page, you can apply to join a Build Team or request to be verified
-				as solo builder and work on projects yourself. <br />
-				It is also the place where you can upload your world when you have finished. To access your
-				page, you need to Sign in. <br />
+				To access this page you need to sign in with your BuildTheEarth Account. <br />
 			</p>
+
 			{router.query.error && (
 				<p style={{ color: theme.colors.red[6] }}>An error occurred, please try again later.</p>
 			)}
+
 			<Button onClick={() => signIn('keycloak')}>Sign in</Button>
+
+			{router.query.callbackUrl && (
+				<Text size="sm" c="dimmed" mt="sm">
+					After Login you will be redirected to {router.query.callbackUrl}
+				</Text>
+			)}
 		</Page>
 	);
 };
 
 export default SignIn;
+
+export async function getStaticProps({ locale }: any) {
+	return {
+		props: {
+			...(await serverSideTranslations(locale, ['common'])),
+		},
+	};
+}
