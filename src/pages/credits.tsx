e58ae600dcbe75541183729e7991e22e49511b17
Nudesuppe42 <jufo.theo@gmail.com>
2024-01-13 17:31:15 +0100

    feat(credits): :zap: Update Credits

--- src/pages/credits.tsx
+++ src/pages/credits.tsx
@@ -1,14 +1,10 @@
 import { Anchor, Container } from '@mantine/core';
-import fetcher, { globalFetcher } from '../utils/Fetcher';
 
-import { GetServerSideProps } from 'next';
 import Link from 'next/link';
 import Page from '../components/Page';
-import { motion } from 'framer-motion';
-import packageJson from '../../package.json';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-const Credits = ({ contributors }: any) => {
+const Credits = () => {
 	return (
 		<Page fullWidth head={{ title: 'Credits' }}>
 			<Container w="80vw" style={{ listStyleType: 'none' }}>
@@ -17,11 +13,11 @@ const Credits = ({ contributors }: any) => {
 				<p>
 					Work on the BuildTheEarth Website is public at{' '}
 					<Anchor component={Link} href={'https://github.com/BuildTheEarth/website-node-backend'}>
-						https://github.com/BuildTheEarth/website-node-backend
+						BuildTheEarth/website-node-backend
 					</Anchor>{' '}
 					and{' '}
 					<Anchor component={Link} href={'https://github.com/BuildTheEarth/website-frontend'}>
-						https://github.com/BuildTheEarth/website-frontend
+						BuildTheEarth/website-frontend
 					</Anchor>
 					. We believe in transparency and would like you to contribute to this project aswell.
 					<br />
@@ -37,6 +33,16 @@ const Credits = ({ contributors }: any) => {
 				</ol>
 				<h2>Inspiration</h2>
 				<ol style={{ listStyleType: 'none' }}>
+					<li>
+						Original Website:{' '}
+						<Anchor component={Link} href={'https://github.com/BuildTheEarth/bte-website'}>
+							BuildTheEarth/bte-website
+						</Anchor>
+						{' © '}
+						<Anchor component={Link} href={'https://github.com/Xesau'}>
+							Xesau
+						</Anchor>
+					</li>
 					<li>
 						Colors and Components:{' '}
 						<Anchor component={Link} href={'https://mantine.dev'}>
@@ -60,10 +66,36 @@ const Credits = ({ contributors }: any) => {
 				<ol style={{ listStyleType: 'none' }}>
 					<li>@aws-sdk/client-s3</li>
 					<li>@bte-germany/terraconvert</li>
+					<li>@keycloak/keycloak-admin-client</li>
+					<li>@prisma/client</li>
+					<li>@turf/helpers</li>
+					<li>@turf/turf</li>
+					<li>blurhash</li>
+					<li>body-parser</li>
+					<li>cors</li>
+					<li>cron</li>
+					<li>dotenv</li>
+					<li>express</li>
+					<li>express-prom-bundle</li>
+					<li>express-session</li>
+					<li>express-validator</li>
+					<li>express-yup-middleware</li>
+					<li>jsonwebtoken</li>
+					<li>keycloak-connect</li>
+					<li>mariadb</li>
+					<li>minimatch</li>
+					<li>multer</li>
+					<li>mysql</li>
+					<li>prom-client</li>
+					<li>reflect-metadata</li>
+					<li>rfdc</li>
+					<li>sharp</li>
+					<li>uuid</li>
+					<li>winston</li>
+					<li>yup</li>
 					<li>@emotion/react</li>
 					<li>@emotion/server</li>
 					<li>@icons-pack/react-simple-icons</li>
-					<li>@keycloak/keycloak-admin-client</li>
 					<li>@mantine/carousel</li>
 					<li>@mantine/core</li>
 					<li>@mantine/dates</li>
@@ -80,7 +112,6 @@ const Credits = ({ contributors }: any) => {
 					<li>@mantine/tiptap</li>
 					<li>@mapbox/mapbox-gl-draw</li>
 					<li>@next/bundle-analyzer</li>
-					<li>@prisma/client</li>
 					<li>@react-three/drei</li>
 					<li>@react-three/fiber</li>
 					<li>@tabler/icons-react</li>
@@ -93,53 +124,35 @@ const Credits = ({ contributors }: any) => {
 					<li>@tiptap/pm</li>
 					<li>@tiptap/react</li>
 					<li>@tiptap/starter-kit</li>
-					<li>@turf/helpers</li>
-					<li>@turf/turf</li>
+					<li>@types/mapbox__mapbox-gl-draw</li>
+					<li>@types/sanitize-html</li>
 					<li>axios</li>
-					<li>blurhash</li>
-					<li>body-parser</li>
-					<li>cors</li>
-					<li>dayjs</li>dotenv
+					<li>clsx</li>
+					<li>cookies-next</li>
+					<li>dayjs</li>
 					<li>embla-carousel-react</li>
 					<li>eslint-import-resolver-webpack</li>
-					<li>express</li>
-					<li>express-prom-bundle</li>
-					<li>express-session</li>
-					<li>express-validator</li>
-					<li>express-yup-middleware</li>
 					<li>flag-icons</li>
 					<li>framer-motion</li>
 					<li>geolib</li>
 					<li>i18next</li>
-					<li>jsonwebtoken</li>
-					<li>keycloak-js</li>
-					<li>keycloak-connect</li>
+					<li>keycloak-j</li>
+					<li>mantine-contextmenu</li>
 					<li>mapbox-gl</li>
 					<li>mapbox-gl-style-switcher</li>
-					<li>mariadb</li>
-					<li>minimatch</li>
-					<li>multer</li>
-					<li>mysql</li>
 					<li>next</li>
 					<li>next-auth</li>
 					<li>next-i18next</li>
 					<li>next-seo</li>
 					<li>oauth-pkce</li>
-					<li>prom-client</li>
 					<li>react</li>
 					<li>react-dom</li>
 					<li>react-i18next</li>
 					<li>react-map-gl</li>
-					<li>reflect-metadata</li>
-					<li>rfdc</li>
 					<li>sanitize-html</li>
-					<li>sharp</li>
 					<li>swr</li>
 					<li>tabler-icons-react</li>
 					<li>three</li>
-					<li>uuid</li>
-					<li>winston</li>
-					<li>yup</li>
 					<li>vague-time</li>
 				</ol>
 				<p>
